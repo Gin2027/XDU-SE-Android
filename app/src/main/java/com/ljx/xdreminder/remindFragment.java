d2fc4bf9b4a9bb641c83daa188418404dc241611
Gin2027 <952462084@qq.com>
2019-03-26 23:33:53 +0800

    First

--- app/src/main/java/com/ljx/xdreminder/remindFragment.java
+++ app/src/main/java/com/ljx/xdreminder/remindFragment.java
@@ -9,10 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 public class remindFragment extends Fragment {
     private Spinner spinner1;
+    private Spinner spinner2;
+    private Spinner spinner3;
+    private Spinner spinner4;
+    private RadioGroup radioGroup;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,11 +29,30 @@ public class remindFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        radioGroup = getActivity().findViewById(R.id.chooseLevel);
         spinner1 = getActivity().findViewById(R.id.spinner1);
+        spinner2 = getActivity().findViewById(R.id.spinner2);
+        spinner3 = getActivity().findViewById(R.id.spinner3);
+        spinner4 = getActivity().findViewById(R.id.spinner4);
+
         String[] Items = getResources().getStringArray(R.array.ways);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_dropdown_item,Items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+        spinner3.setAdapter(adapter);
+        spinner4.setAdapter(adapter);
+
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                int id = radioGroup.getCheckedRadioButtonId();
+                RadioButton radioButton = getActivity().findViewById(id);
+                final String level = radioButton.getText().toString();
+            }
+        });
+
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
